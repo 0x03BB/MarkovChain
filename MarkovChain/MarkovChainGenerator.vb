e412e8c6ea989b6e9fa72463bc7e6aba8c1ec1be
Michael Fieldness <mfieldness@gmail.com>
2017-08-18 13:41:31 -0700

    Finished MarkovChainGenerator.

--- MarkovChain/MarkovChainGenerator.vb
+++ MarkovChain/MarkovChainGenerator.vb
@@ -3,7 +3,7 @@
         Dim reg = New Text.RegularExpressions.Regex("([-\w]+|[.,;:])", Text.RegularExpressions.RegexOptions.Compiled)
         Dim captures = reg.Matches(corpus)
 
-        Dim dic = New SortedDictionary(Of String, SortedDictionary(Of String, Integer))
+        Dim dic = New SortedDictionary(Of String, Dictionary(Of String, Integer))
 
         For i = 0 To captures.Count - 2
             Dim currentString = captures(i).Value
@@ -17,11 +17,31 @@
                     existingEntry.Add(nextString, 1)
                 End If
             Else
-                Dim newEntry = New SortedDictionary(Of String, Integer) From {{nextString, 1}}
+                Dim newEntry = New Dictionary(Of String, Integer) From {{nextString, 1}}
                 dic.Add(currentString, newEntry)
             End If
         Next
 
+        Dim strings = dic.Keys.ToArray
 
+        Dim lookup = New Dictionary(Of String, Integer)
+        For i = 0 To dic.Keys.Count - 1
+            lookup.Add(strings(i), i)
+        Next
+
+        Dim chances = New Double(dic.Keys.Count - 1, dic.Keys.Count - 2) {}
+
+        For Each startingPair In dic
+            Dim y = lookup(startingPair.Key)
+            Dim total = startingPair.Value.Values.Sum
+            For Each targetPair In startingPair.Value
+                Dim x = lookup(targetPair.Key)
+                If x <> dic.Keys.Count - 1 Then
+                    chances(y, x) = targetPair.Value / total
+                End If
+            Next
+        Next
+
+        Return New MarkovChain(strings, chances)
     End Function
 End Module
